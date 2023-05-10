911588b7acce5d512ab61955ba316daf5300c80b
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-05-09 22:34:29 -0400

    First commit

---
+++ react-version/src/components/TweetForm.js
@@ -0,0 +1,19 @@
+function TweetForm() { 
+  return (
+    <main className="container">
+      <section className="newtweet">
+        <form method="post" action="/tweets" className="newtweet__form">
+          <textarea
+            className="form__textarea"
+            name="text"
+            placeholder="What are you humming about?"
+          ></textarea>
+          <input type="submit" value="Tweet" className="form__input" />
+          <span className="form__counter">140</span>
+        </form>
+      </section>
+    </main>
+  );
+}
+
+export default TweetForm;
